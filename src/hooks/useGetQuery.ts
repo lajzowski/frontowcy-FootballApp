57af592376d8a9aeb510a0a25e700e87a5986755
Marcin Woźnica <poczta@marcin.wroclaw.pl>
2025-02-12 22:19:28 +0100

    Poprawiono: Hook do pobierania danych. Teraz tworzy poprawnie tablice key na podstawie linku ze slashem.

--- src/hooks/useGetQuery.ts
+++ src/hooks/useGetQuery.ts
@@ -1,10 +1,15 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
-export const useGetQuery = <T>(queryKey: string): UseQueryResult<T> => {
+export const useGetQuery = <T>(
+  queryKey: string,
+  relation?: 'players' | 'teams' | 'games'
+): UseQueryResult<T> => {
   return useQuery<T>({
     queryKey: queryKey.includes('/') ? queryKey.split('/') : [queryKey],
     queryFn: async () => {
-      const data = await fetch(`http://localhost:3000/${queryKey}`);
+      const data = await fetch(
+        `http://localhost:3000/${queryKey}${relation ? `?_embed=${relation}` : ''}`
+      );
       return data.json();
     },
   });
