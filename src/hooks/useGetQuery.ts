486afbd1f58441f9ce3f55b83393bbf83d92a61b
Marcin Woźnica <poczta@marcin.wroclaw.pl>
2025-02-13 16:23:15 +0100

    Dodano: Dodano sortowanie do useGetQuery.ts

--- src/hooks/useGetQuery.ts
+++ src/hooks/useGetQuery.ts
@@ -2,13 +2,14 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 export const useGetQuery = <T>(
   queryKey: string,
-  relation?: 'players' | 'teams' | 'games'
+  relation?: 'players' | 'teams' | 'games',
+  sortBy?: string
 ): UseQueryResult<T> => {
   return useQuery<T>({
     queryKey: queryKey.includes('/') ? queryKey.split('/') : [queryKey],
     queryFn: async () => {
       const data = await fetch(
-        `http://localhost:3000/${queryKey}${relation ? `?_embed=${relation}` : ''}`
+        `http://localhost:3000/${queryKey}${relation ? `?_embed=${relation}` : ''}${sortBy ? `?_sort=${sortBy}` : ''}`
       );
       return data.json();
     },
